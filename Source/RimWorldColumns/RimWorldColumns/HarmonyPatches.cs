ca9e391faf4b2afc2b389ff626f815a4b6ab8a47
Maxim <MaximSw@web.de>
2021-08-16 12:03:10 +0200

    Patch Cleanup

--- Source/RimWorldColumns/RimWorldColumns/HarmonyPatches.cs
+++ Source/RimWorldColumns/RimWorldColumns/HarmonyPatches.cs
@@ -2,89 +2,77 @@
 using RimWorld;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Verse;
 
 namespace RimWorldColumns
 {
     [StaticConstructorOnStartup]
-    class Main
+    public static class HarmonyPatches
     {
-        public static Pawn ChoicesForPawn = null;
-
-        static Main()
+        static HarmonyPatches()
         {
+            Log.Message("[Utility Columns] - Patching...");
             var harmony = new Harmony("nephlite.orbitaltradecolumn");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-            LongEventHandler.QueueLongEvent(new Action(Init), "LibraryStartup", false, null);
+            VEPatch();
+            //LongEventHandler.QueueLongEvent(new Action(VEPatch), "LibraryStartup", false, null);
         }
 
-        private static void Init()
+        private static void VEPatch()
         {
-            // Vanilla Royalty Support
-            if (ModLister.GetActiveModWithIdentifier("OskarPotocki.VanillaExpanded.RoyaltyPatches") != null)
+            // Patch VE - RoyaltyPatches
+            if (ModLister.GetActiveModWithIdentifier("OskarPotocki.VanillaExpanded.RoyaltyPatches") == null) return;
+            foreach (var d in DefDatabase<RoyalTitleDef>.AllDefs)
             {
-                bool patched = false;
-                foreach (var d in DefDatabase<RoyalTitleDef>.AllDefs)
+                if(d == null) continue;
+                if (d.bedroomRequirements != null)
                 {
-                    if (d.bedroomRequirements != null)
+                    foreach (var c in d.bedroomRequirements)
                     {
-                        foreach (var c in d?.bedroomRequirements)
-                            if (c is RoomRequirement_ThingAnyOf rr && AddColumns(rr))
-                                patched = true;
-                    }
-                    if (d.throneRoomRequirements != null)
-                    {
-                        foreach (var c in d?.throneRoomRequirements)
-                            if (c is RoomRequirement_ThingAnyOf rr && AddColumns(rr))
-                                patched = true;
-
+                        if (c is RoomRequirement_ThingAnyOf rr)
+                            AddColumns(ref rr);
                     }
                 }
-                if (patched)
-                    Log.Message("[Utility Columns] Successfully patched Vanially Expanded - Royaltys Patch");
-                else
-                    Log.Error("[Utility Columns] Failed to patch Vanially Expanded - Royaltys Patch");
+                if (d.throneRoomRequirements != null)
+                {
+                    foreach (var c in d.throneRoomRequirements)
+                    {
+                        if (c is RoomRequirement_ThingAnyOf rr)
+                            AddColumns(ref rr);
+                    }
+
+                }
             }
+            
+            /* 
+            if (patched)
+                Log.Message("[Utility Columns] Successfully patched Vanially Expanded - Royaltys Patch");
+            else
+                Log.Error("[Utility Columns] Failed to patch Vanially Expanded - Royaltys Patch");
+            */
         }
 
-        private static bool AddColumns(RoomRequirement_ThingAnyOf rr)
+        private static void AddColumns(ref RoomRequirement_ThingAnyOf rr)
         {
-            if (rr.things.Contains(DefOf.Column))
+            if (rr.things.Contains(UCDefOf.Column))
             {
                 rr.things.AddRange(ColumnDefs);
-                return true;
             }
-            return false;
         }
 
-        public static IEnumerable<ThingDef> ColumnDefs => new List<ThingDef>()
+        public static List<ThingDef> ColumnDefs => new List<ThingDef>()
         {
-            DefOf.LightColumnMod,
-            DefOf.DarkColumnMod,
-            DefOf.OrbitalTradeColumnMod,
-            DefOf.SunColumnMod,
-            DefOf.IceColumnMod,
-            DefOf.DetColumnMod,
-            DefOf.FlameColumnMod,
-            DefOf.GraveColumnMod,
+            UCDefOf.LightColumnMod,
+            UCDefOf.DarkColumnMod,
+            UCDefOf.OrbitalTradeColumnMod,
+            UCDefOf.SunColumnMod,
+            UCDefOf.IceColumnMod,
+            UCDefOf.DetColumnMod,
+            UCDefOf.FlameColumnMod,
+            UCDefOf.GraveColumnMod,
         };
-    }
-
-    [RimWorld.DefOf]
-    public static class DefOf
-    {
-        public static ThingDef LightColumnMod;
-        public static ThingDef DarkColumnMod;
-        public static ThingDef OrbitalTradeColumnMod;
-        public static ThingDef SunColumnMod;
-        public static ThingDef IceColumnMod;
-        public static ThingDef DetColumnMod;
-        public static ThingDef FlameColumnMod;
-        public static ThingDef GraveColumnMod;
-
-        // Base game column def
-        public static ThingDef Column;
     }
 
     [HarmonyPatch(typeof(RoomRequirement_ThingCount), "Count")]
@@ -92,11 +80,9 @@ namespace RimWorldColumns
     {
         static void Postfix(RoomRequirement_ThingCount __instance, ref int __result, Room r)
         {
-            if (__instance.thingDef == DefOf.Column)
-            {
-                foreach(var def in Main.ColumnDefs)
-                    __result += r.ThingCount(def);
-            }
+            if (__instance.thingDef != UCDefOf.Column) return;
+
+            __result += HarmonyPatches.ColumnDefs.Sum(r.ThingCount);
         }
     }
 }
